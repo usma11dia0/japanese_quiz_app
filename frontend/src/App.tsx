f209a9b9e48aaff82f8f7646a90b7d96f3613f02
usma11dia0 <usma11dia0@gmail.com>
2022-06-22 13:41:37 +0900

    create pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+import { FinalScreen } from "./pages/FinalScreen";
+import { Questions } from "./pages/Questions";
+import { Settings } from "./pages/Settings";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route path="/" element={<Settings />} />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScreen />} />
+          </Routes>
+        </Box>
+      </Container>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Settings.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const Settings = () => {
+  return (
+    <>
+      <Typography variant="h2" fontWeight="bold">
+        Quiz App
+      </Typography>
+      <div>Settings</div>
+    </>
+  );
+};
