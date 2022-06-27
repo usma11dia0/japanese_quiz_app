1975eee69b5b51113411df9fcc824d36580b1b64
usma11dia0 <usma11dia0@gmail.com>
2022-06-27 19:57:18 +0900

    complete build quiz app with trivia api

--- frontend/src/pages/FinalScreen.tsx
+++ frontend/src/pages/FinalScreen.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { selectScore } from "../redux/reducer";
+import { useNavigate } from "react-router-dom";
+import { handleAmountChange, handleScoreChange } from "../redux/actions";
 
 export const FinalScreen = () => {
-  return <div>FinalScreen</div>;
+  const { score } = useSelector(selectScore);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(50));
+    navigate("/");
+  };
+
+  return (
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">
+        back to settings!
+      </Button>
+    </Box>
+  );
 };
--- frontend/src/pages/Questions.tsx
+++ frontend/src/pages/Questions.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { decode } from "html-entities";
+import { useNavigate } from "react-router-dom";
 
 import { QUIZ, TState } from "../features/types";
 import { useAxios } from "../hooks/useAxios";
@@ -10,7 +12,9 @@ import {
   selectQuestionCategory,
   selectQuestionDifficulty,
   selectQuestionType,
+  selectScore,
 } from "../redux/reducer";
+import { handleScoreChange } from "../redux/actions";
 
 const getRandomInt = (max: number) => {
   return Math.floor(Math.random() * Math.floor(max));
@@ -21,6 +25,10 @@ export const Questions = () => {
   const question_difficulty = useSelector(selectQuestionDifficulty);
   const question_type = useSelector(selectQuestionType);
   const amount_of_question = useSelector(selectAmoutOfQuestion);
+  const score = useSelector(selectScore);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   let apiUrl = `/api.php?amount=${amount_of_question}`;
   if (question_category) {
@@ -40,6 +48,8 @@ export const Questions = () => {
   const [questionIndex, setQuestionIndex] = useState<number>(0);
   const [options, setOptions] = useState<Array<string>>([]);
 
+  console.log(options);
+
   useEffect(() => {
     if (response?.results.length) {
       const question = response.results[questionIndex];
@@ -49,8 +59,9 @@ export const Questions = () => {
         0,
         question.correct_answer
       );
+      setOptions(answers);
     }
-  }, [response]);
+  }, [response, questionIndex]);
 
   if (loading) {
     return (
@@ -60,20 +71,41 @@ export const Questions = () => {
     );
   }
 
+  const handleClickAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const question = response.results[questionIndex];
+    if (e.target instanceof HTMLElement) {
+      if (
+        e.target.innerText.toUpperCase() ===
+        question.correct_answer.toUpperCase()
+      ) {
+        dispatch(handleScoreChange(score + 1));
+      }
+    }
+
+    if (questionIndex + 1 < response.results.length) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      navigate("/score");
+    }
+  };
+
   return (
     <Box>
       <Typography variant="h4">Questions {questionIndex + 1} </Typography>
       <Typography mt={5}>
-        {response!.results[questionIndex].question}
+        {decode(response?.results[questionIndex].question)}
       </Typography>
-      <Box mt={2}>
-        <Button variant="contained">Answer 1</Button>
-      </Box>
-      <Box mt={2}>
-        <Button variant="contained">Answer 2</Button>
-      </Box>
+      {options.map((data, id) => (
+        <Box mt={2} key={id}>
+          <Button onClick={(e) => handleClickAnswer(e)} variant="contained">
+            {decode(data)}
+          </Button>
+        </Box>
+      ))}
       <Box mt={5}>
-        <Button>Score 2 / 6</Button>
+        <Button>
+          Score {score} / {response.results.length}
+        </Button>
       </Box>
     </Box>
   );
