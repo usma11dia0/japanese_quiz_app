b8ca27ddb6f231c4dbdff797eedafd490d2b1ebf
usma11dia0 <usma11dia0@gmail.com>
2022-06-28 10:23:04 +0900

    install black

--- backend/japanese_quiz_api/wsgi.py
+++ backend/japanese_quiz_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'japanese_quiz_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "japanese_quiz_api.settings")
 
 application = get_wsgi_application()
